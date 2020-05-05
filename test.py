213823a93b5833d271eab6d150b90f326e2245e0
HilJiang <62001653+HilJiang@users.noreply.github.com>
2020-05-05 14:07:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,66 +1,91 @@
-    
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+import math
 
-def entropy(s):    
-    _, counts = np.unique(list(s), return_counts=True)
-    total = sum(counts)
-    percent = list(map(lambda x: x / total, counts))
-    return sum(-n * np.log(n) for n in percent)
-    
-def processDomain(s):
-    return len(s), sum(c.isdigit() for c in s), entropy(s)
-
+domainlist = []
 class Domain:
-    def __init__(self, _domain, _label):
-        self.domain = _domain
-        self.label = _label
-        self.domainNameLength, self.domainNumberCount, self.letterEntropy = processDomain(self.domain)
-    
-    def returnData(self):
-        return [self.domainNameLength, self.domainNumberCount, self.letterEntropy]
+	def __init__(self,_name,_label,_length,_numbers,_entropy):
+		self.name = _name
+		self.label = _label
+		self.length=_length
+		self.numbers=_numbers
+		self.entropy=_entropy
+		
 
-    def returnLabel(self):
-        if self.label == "notdga":
-            return 0
-        else:
-            return 1
+	def returnData(self):
+		return [self.length, self.numbers, self.entropy]
 
-domainList = []      
-
+	def returnLabel(self):
+		if self.label == "notdga":
+			return 0
+		else:
+			return 1
+		
 def initData(filename):
-    with open(filename) as f:
-        for line in f:
-            line = line.strip()
-            if line == "":
-                continue
-            tokens = line.split(",")
-            domain = tokens[0]
-            label = tokens[1]
-            domainList.append(Domain(domain, label))
+	with open(filename) as f:
+		for line in f:
+			line = line.strip()
+			if line.startswith("#") or line =="":
+				continue
+			tokens = line.split(",")
+			name = tokens[0]
+			label = tokens[1]
+			length= len(name)
+			num=0
+			entro=0     #entropy
+			result={}   #times of every character appearing
+			frequency={}#frequency of every character appearing
+			for i in name:
+                            result[i]=name.count(i)
+                            frequency[i]=float(result[i])/length
+                            entro-=1*(frequency[i])*math.log(frequency[i],2)
+                            if i.isdigit():
+                                num = num + 1
+			numbers= num  
+			entropy = entro
+			domainlist.append(Domain(name,label,length,numbers,entropy))
 
-def initTest(filename):
-    with open(filename) as f:
-        return list(filter(lambda line: line != "", map(lambda line: line.strip(), f)))
+def main():
+	initData("train.txt")
+	featureMatrix = []
+	labelList = []
+	for item in domainlist:
+		featureMatrix.append(item.returnData())
+		labelList.append(item.returnLabel())
+
+	clf = RandomForestClassifier(random_state=0)
+	clf.fit(featureMatrix,labelList)
+
+	file1=open("test.txt")
+	file2=open("result.txt","w")
+	for line in file1:
+		line = line.strip()
+		if line.startswith("#") or line =="":
+			continue
+		tokens_test = line.split(",")
+		name_test = tokens_test[0]
+		length_test = len(name_test)
+		num_test=0
+		entro_test=0     #entropy
+		result_test={}   #times of every character appearing
+		frequency_test={}#frequency of every character appearing
+		for i in name_test:
+                	result_test[i]=name_test.count(i)
+                	frequency_test[i]=float(result_test[i])/length_test
+                	entro_test-=1*(frequency_test[i])*math.log(frequency_test[i],2)
+                	if i.isdigit():
+                	    num_test +=1
+		numbers_test= num_test  
+		entropy_test = entro_test
+		if clf.predict([[length_test,numbers_test,entropy_test]])==0:
+		    label_test1 = "notdga"
+		    print(name_test,label_test1,file=file2)
+		if clf.predict([[length_test,numbers_test,entropy_test]])==1:
+		    label_test2 = "dga"
+		    print(name_test,label_test2,file=file2)
+
+	file1.close
+	file2.close
 
 if __name__ == '__main__':
-    initData("train.txt")
-    featureMatrix = list(map(lambda domain: domain.returnData(), domainList))
-    labelList = list(map(lambda domain: domain.returnLabel(), domainList))
-    testDomains = initTest("test.txt")
-    testDomainFeatures = []
-    for domain in testDomains:
-        domainNameLength, domainNumberCount, letterEntropy = processDomain(domain)
-        testDomainFeatures.append([domainNameLength, domainNumberCount, letterEntropy])
-    clf = RandomForestClassifier(random_state=0)
-    clf.fit(featureMatrix, labelList)
-    testLabels = clf.predict(testDomainFeatures)
-    output = list(zip(testDomains, testLabels))
-    with open("result.txt", "w+") as f:
-        for domain, label in output:
-            line = domain + ","
-            if label == 0:
-                line = line + "notdga\n"
-            else:
-                line = line + "dga\n"
-            f.write(line)
+	main()
